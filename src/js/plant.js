1829cfe04095f56aa37bfb676276a4973a55ec8f
Jase Seeley <jcseeley31590@yahoo.com>
2022-04-13 11:39:24 -0700

    add code from lessons.
    Co-authored-by: Ryan Ashby <rjapsu160@gmail.com>

--- src/js/plant.js
+++ src/js/plant.js
@@ -1,8 +1,3 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-
 // This function stores our state.
 
 const storeState = () => {
@@ -11,8 +6,8 @@ const storeState = () => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
     return newState;
-  };
-};
+  }
+}
 
 const stateControl = storeState();
 
@@ -23,15 +18,14 @@ const changeState = (prop) => {
     return (state) => ({
       ...state,
       [prop] : (state[prop] || 0) + value
-    });
-  };
-};
+    })
+  }
+}
 
 // We create four functions using our function factory. We could easily create many more.
 
-// const feed = changeState("soil")(1);
-const blueFood = changeState("soil")(5); 
-const redFood = changeState("soil")(8);
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
 
 const hydrate = changeState("water")(1);
 const superWater = changeState("water")(5);
@@ -41,30 +35,15 @@ $(document).ready(function() {
 // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
 
   $('#feed').click(function() {
-    const changeState = stateControl(blueFood);
-    $('#soil-value').text(`Soil: ${changeState.soil}`);
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
   });
 
-  $('#feedred').click(function() {
-    const changeState = stateControl(redFood);
-    $('#soil-value').text(`Soil: ${changeState.soil}`);
-  });
-
-  $('#water').click(function() {
-    const newState = stateControl(hydrate);
-    $('#water-value').text(`Water: ${newState.water}`);
-  });
-
-  $('#super-water').click(function() {
-    const newState = stateControl(superWater);
-    $('#water-value').text(`Water: ${newState.water}`);
-  });
 // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
 
   $('#show-state').click(function() {
     // We just need to call stateControl() without arguments to see our current state.
     const currentState = stateControl();
-    $('#soil-value').text(`This is the current soil stat: ${currentState.soil}`);
-    $('#water-value').text(`This is the current water stat: ${currentState.water}`);
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
   });
 });
